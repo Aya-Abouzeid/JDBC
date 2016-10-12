0f8acde31e7b6acc1ec7ead96137e434b21ce8cd
aya aly saad abouzeid <aya_aly_abouzeid@yahoo.com>
2016-10-12 23:10:51 +0200

    calculator-2

--- calculator/cs02/MyCalculator.java
+++ calculator/cs02/MyCalculator.java
@@ -4,9 +4,12 @@ import eg.edu.alexu.csd.oop.calculator.Calculator;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.io.*;
+import java.lang.*;
+import java.util.*;
 import javax.script.ScriptEngine;
 
 public class MyCalculator implements Calculator {
+
 
 	/* Take input mathematical expression from user */
 	   ScriptEngineManager manager = new ScriptEngineManager();
@@ -14,9 +17,7 @@ public class MyCalculator implements Calculator {
 	    String exp;
 	    String current;
 	    int counter = 0;
-//	    node head = new node (null);
-//	    node tail = new node (null);
-//	    node order = new node(null);
+
 	    String saved = null;
 	    String order;
 	    int bounds=0;
@@ -27,40 +28,27 @@ public class MyCalculator implements Calculator {
 		exp =s;
 		saved = s + " = " + getResult();
 		if (counter == 0) {
-//			node node = new node(saved);
-//			node.next = null;
-//			node.prev = null;
+
 			A[counter] = saved;
+			System.out.println(counter + "  "+ A[0]);
 			counter++;
-//			head = node;
-//			tail = head;
-//			order = node;
-		} else if (counter < 5) {
-//			node node = new node(saved);
-//			node.next = head;
-//			node.prev = null;
-//			head.prev = node;
-//			head.next = null;
-//			tail = head;
-//			head = node;
-			A[counter]=saved;
-			counter++;
-//			order = node;
-		} else {
-//			tail.prev.next = null;
-//			tail = tail.prev;
-//			node node = new node(saved);
-//			node.next = head;
-//			node.prev = null;
-//			head.prev = node;
-//			head.next = null;
-//			head = node;
-//			order = node;
-			for(int i =4 ; i>0 ; i--){
+
+		} else if (counter < 5 && counter > 0) {
+
+			for(int i =counter ; i>0 ; i--)
+			{
 				A[i] = A[i-1];
-				
 			}
-			A[0]=saved;
+			A[0] = saved;
+			System.out.println(counter + "  "+ A[0]);
+			counter++;
+
+		} else {
+
+			for (int i = 4; i > 0; i--) {
+				A[i] = A[i-1];
+			}
+			A[0] = saved;
 
 		}
 
@@ -92,20 +80,21 @@ public class MyCalculator implements Calculator {
 		if (counter == 0) {
 			return null;
 		} else {
-		return A[0];
+		return A[bounds];
 		}
 	}
 	/* Return the last operation in String format,
 	 * or Null if no more history available */
 
 	@Override
-	
+
 	public String prev() {
 		// TODO Auto-generated method stub
-		if (counter == 0 || order.equals(A[4])) {
+	if ((bounds + 1) > counter || (bounds + 1) > 4) {
 			return null;
 		} else {
-		order = A[bounds+1];
+			bounds++;
+		order = A[bounds];
 		return order;
 		}
 	}
@@ -115,45 +104,37 @@ public class MyCalculator implements Calculator {
 	@Override
 	public String next() {
 		// TODO Auto-generated method stub
-		order = A[bounds-1];
 
-		if (counter == 0 ) {
+
+		if ((bounds - 1) < 0 ) {
 			return null;
-		} else if(!order.equals(A[0])){
-		return order;
-		}
-		else{
+		} else {
+			bounds--;
+			order = A[bounds];
 			return order;
 		}
 	}
 	/* Save the last 5 done Operations in a file*/
+	private Formatter x;
 
 	@Override
 	public void save() {
 		// TODO Auto-generated method stub
-//		try
-//		 {
-//		    File file = new File( save );
-//
-//		    // if file doesnt exists, then create it 
-//		    if ( ! file.exists( ) )
-//		    {
-//		        file.createNewFile( );
-//		    }
-//
-//		    FileWriter fw = new FileWriter( file.getAbsoluteFile( ) );
-//		    BufferedWriter bw = new BufferedWriter( fw );
-//		    bw.write( text );
-//		    bw.close( );
-//		    //System.out.println("Done writing to " + fileName); //For testing 
-//		 }
-//		 catch( IOException e )
-//		 {
-//		 System.out.println("Error: " + e);
-//		 e.printStackTrace( );
-//		 }
+
+		try {
+			x = new Formatter("save.txt");
+
+			for (int i = 0; i < 5; i++) {
+
+				x.format(A[i]);
+			}
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+      x.close();
 
 	}
+
 	/* Load from file the saved 5 operations */
 
 	@Override
