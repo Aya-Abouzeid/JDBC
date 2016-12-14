ef2ccf276fb39b6458580ade6002a3b52a120a65
aya aly saad abouzeid <aya_aly_abouzeid@yahoo.com>
2016-12-14 20:12:04 +0200

    delete edited

--- DBMS/JsonTable.java
+++ DBMS/JsonTable.java
@@ -38,21 +38,13 @@ public class JsonTable implements ITable{
 	public String[] getType(){
 		  return this.Type;
 	  }
-	protected boolean detectTypes(String[]properties){
-		 for(int i = 0; i < properties.length; i++){
-		    	String[] str = properties[i].split(" ");
-		    	if(!(str[0].equals("int"))&&!(str[0].equals("float"))&&!(str[0].equals("date"))&&!(str[0].equals("varchar"))){
-		    		return false;
-		    	}
-		 }
-		return true;
-	}
+	
 	@Override
     public void creatTable(String databaseName, String tableName, String[] properties) {
 		System.out.println("jsonpath"+path);
 		File jsonFile  = new File(path + File.separator + databaseName+File.separator+tableName+".json");
 		try {
-		if(detectTypes(properties)){
+			
 				
 			
 			if (jsonFile.createNewFile()){System.out.println("File is created!");
@@ -70,8 +62,7 @@ public class JsonTable implements ITable{
 			file.newLine();
 			file.write("}");
 			file.close();
-	}
-		}catch (IOException e) {e.printStackTrace();}
+		 }catch (IOException e) {e.printStackTrace();}
 	}
  
 	private void write(File jsonFile ,String tableName){
@@ -113,7 +104,7 @@ public class JsonTable implements ITable{
 	public ArrayList<ArrayList<String>> readFile(String databaseName, String tableName) {
 		ArrayList<ArrayList<String>> tableData = new ArrayList<ArrayList<String>>();
 		File tables = new File(path + File.separator + databaseName+File.separator+tableName+".json");
-		ArrayOfTypes = column(databaseName , tableName);
+		//ArrayOfTypes = column(databaseName , tableName);
 		ArrayList<String> trim = new ArrayList<String>();
 		ArrayList<ArrayList<String>> tablelines = new ArrayList<ArrayList<String>>();
 		try{trim = readline(tables,tableName);
@@ -161,25 +152,10 @@ public class JsonTable implements ITable{
 		} catch (IOException e) {e.printStackTrace();}
 		return trim;
 	}
-	private String []  column(String databaseName , String tableName){
-		File tables = new File(path+ File.separator + databaseName+File.separator+tableName+".json");
-		ArrayList<String> trim = new ArrayList<String>();
-		ArrayList<String> t = new ArrayList<String>();
-			trim = readline(tables,tableName);
-            for(int i = 0; i < trim.size(); i++){
-				if(trim.get(i).contains("int")||trim.get(i).contains("float")||trim.get(i).contains("date")||trim.get(i).contains("varchar"))
-				{
-					String[] typestable = trim.get(i).split(":");
-					typestable[0] = typestable[0].replace("\"", "");
-					t.add(typestable[0]);}}
-    		for(int i = 0;i < t.size();i++){
-    			ArrayOfTypes[i]=t.get(i);}
-		return ArrayOfTypes;
-	}
  
 	private void writebegin(String databaseName,File jsonFile ,String size ,String tableName ){
 		jsonFile.delete();
-		ArrayOfTypes = column(databaseName , tableName);
+		//ArrayOfTypes = column(databaseName , tableName);
 		try{jsonFile.createNewFile();
 			file = new BufferedWriter(new FileWriter(jsonFile));
             file.write("{");
