b5a441fb1a67d1861c265bd299e7414b6c7b54b5
aya aly saad abouzeid <aya_aly_abouzeid@yahoo.com>
2016-12-16 18:17:29 +0200

    log4j edited

--- jdbc/Log4j.java
+++ jdbc/Log4j.java
@@ -1,15 +1,16 @@
 package eg.edu.alexu.csd.oop.jdbc;
 
 import java.io.File;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Log4j {
-	 public Logger projectLog(){
-		 Logger logger = Logger.getLogger(Log4j.class);
-		 String log4jConfigFile = System.getProperty("user.home")
-	             + File.separator + "log4j.properties";
-	     PropertyConfigurator.configure(log4jConfigFile);
-	     return logger;
-	 }
+//	 public Logger projectLog(){
+//		 Logger logger = Logger.getLogger(Log4j.class);
+//		 String log4jConfigFile = System.getProperty("user.home")
+//	             + File.separator + "log4j.properties";
+//	     PropertyConfigurator.configure(log4jConfigFile);
+//	     return logger;
+//	 }
 }
